08a4cbc482903c5da16918b336a32afa70268f2d
Alex Kravets <montana14@yandex.ru>
2019-03-27 11:24:48 +0300

    6 tast year fix

--- 6/6_reg_exp_solution.php
+++ 6/6_reg_exp_solution.php
@@ -15,7 +15,7 @@ function timeCheckReg(string $foo)
 {
     if (!empty($foo)) {
         $regExp = '/^(?:(?:(?:(?:0[1-9]|1[0-9]|2[0-8])[\/](?:0[1-9]|1[012]))|(?:(?:29|30|31)[\/](?:0[13578]|1[02]))' .
-        '|(?:(?:29|30)[\/](?:0[4,6,9]|11)))[\/](?:19|[2-9][0-9])\d\d)|(?:29[\/]02[\/](?:19|[2-9][0-9])' .
+        '|(?:(?:29|30)[\/](?:0[4,6,9]|11)))[\/](?:1[6-9]|[2-9][0-9])\d\d)|(?:29[\/]02[\/](?:1[6-9]|[2-9][0-9])' .
         '(?:00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96))$/u';
 
         preg_match($regExp, $foo) ? $result = true : $result = false;
@@ -27,8 +27,8 @@ function timeCheckReg(string $foo)
 }
 
 echo "<pre>";
-var_dump(timeCheckReg('29/02/2000'));
-var_dump(timeCheckReg('30/04/2003'));
+var_dump(timeCheckReg('23/02/9999'));
+var_dump(timeCheckReg('30/04/1600'));
 var_dump(timeCheckReg('01/01/2003'));
 var_dump(timeCheckReg('29/02/2001'));
 var_dump(timeCheckReg('30-04-2003'));
